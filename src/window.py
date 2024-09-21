8e48e4fbc5780df9b4a842ff5afc47680eb92d71
mitchellkolb <mkolb321@gmail.com>
2024-09-20 23:22:26 -0700

    Made open button clickable and linked to a function in window.py

--- src/window.py
+++ src/window.py
@@ -1,13 +1,20 @@
 
 
-from gi.repository import Adw
-from gi.repository import Gtk
+from gi.repository import Adw, Gio, Gtk
 
 @Gtk.Template(resource_path='/com/example/TextEditor/window.ui')
 class TextEditorGtkWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'TextEditorGtkWindow'
 
     main_text_view = Gtk.Template.Child()
+    open_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        open_action = Gio.SimpleAction(name="open")
+        open_action.connect("activate", self.open_file_dialog)
+        self.add_action(open_action)
+
+    def open_file_dialog(self, action, _):
+        pass
